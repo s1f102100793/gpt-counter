e96f7d08401daf9a93f8c1034e7b683629934a98
s1f102100793 <s1f102100793@iniad.org>
2024-02-28 16:38:18 +0900

    update: options/index.tsx storage sample

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,12 +1,46 @@
-import { useState } from "react"
+import {  useForm, type SubmitHandler } from "react-hook-form"
+import { useStorage } from "@plasmohq/storage/hook"
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
+  const [name, setName] = useStorage("name")
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<{ name: string }>()
+
+  const onSubmit: SubmitHandler<{ name: string }> = (data) => {
+    setName(data.name)
+  }
 
   return (
     <div>
       <h1>This is the Option UI page!</h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Name:
+          <input
+            {...register("name", {
+              required: "This is required"
+            })}
+            defaultValue={name}
+            style={{
+              border: errors.name ? "1px solid red" : "1px solid black"
+            }}
+          />
+        </label>
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
+
+        <button
+          type="submit"
+          style={{
+            marginTop: "20px",
+            display: "block"
+          }}>
+          Save
+        </button>
+      </form>
     </div>
   )
 }
