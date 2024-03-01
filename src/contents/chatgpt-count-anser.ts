0937c24f61440811a4769a35dc41026cc61e49d5
s1f102100793 <s1f102100793@iniad.org>
2024-03-01 12:46:59 +0900

    add: getElementsAfterDelay func

--- src/contents/chatgpt-count-anser.ts
+++ src/contents/chatgpt-count-anser.ts
@@ -1,4 +1,18 @@
-import { get } from "http"
+export const config = {
+  matches: ["https://chat.openai.com/*"],
+  all_frames: true
+}
+
+const getElementsAfterDelay = (): Promise<number> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const elements = document.querySelectorAll(
+        ".relative.flex.w-full.flex-col.agent-turn"
+      )
+      resolve(elements.length)
+    }, 1000)
+  })
+}
 
 let currentCount: number = 0
 
@@ -15,11 +29,9 @@ const mutationCallback: MutationCallback = (
 }
 
 const searchForClass = (node: Node): number => {
-  // console.log(node)
   let count = 0
   if (node.nodeType === Node.ELEMENT_NODE) {
     const element = node as Element
-    // console.log("Element: ", element)
     if (element.matches(".relative.flex.w-full.flex-col.agent-turn")) {
       count = 1
     }
@@ -30,28 +42,13 @@ const searchForClass = (node: Node): number => {
   return count
 }
 
-const countClassOccurrencesInContainer = (maxTarget: Element): number => {
-  const htmlContent = maxTarget.innerHTML
-  // 検索するクラス文字列
-  const searchString = 'class="relative flex w-full flex-col agent-turn"'
-  // 正規表現を使って検索し、マッチした回数をカウント
-  const regex = new RegExp(searchString, "g")
-  const matches = htmlContent.match(regex)
-  const count = matches ? matches.length : 0
-  console.log(
-    `Occurrences of the specified class within the container: ${count}`
-  )
-  return count
-}
-
 const observer = new MutationObserver(mutationCallback)
 
-const observeDOMChanges = (): void => {
+const observeDOMChanges = async (): Promise<void> => {
   const maxTarget = document.querySelector(".flex-1.overflow-hidden")
   if (maxTarget) {
-    const initialCount = countClassOccurrencesInContainer(maxTarget) // 初期カウントを実行して結果を取得
-    currentCount = initialCount // 初期カウントの結果をcurrentCountに格納
-    console.log(`Initial count of elements: ${initialCount}`) // 初期カウントの結果をログに出力
+    const initialCount = await getElementsAfterDelay()
+    currentCount = initialCount
     observer.observe(maxTarget, { childList: true, subtree: true })
     console.log("Observing DOM changes...")
   } else {
@@ -60,16 +57,3 @@ const observeDOMChanges = (): void => {
 }
 
 observeDOMChanges()
-
-const getElementsAfterDelay = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const elements = document.querySelectorAll(
-        ".relative.flex.w-full.flex-col.agent-turn"
-      )
-      resolve(elements.length)
-    }, 1000)
-  })
-}
-
-getElementsAfterDelay()
