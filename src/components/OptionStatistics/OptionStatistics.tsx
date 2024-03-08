094ba9b8e60e3b3b784239442cc5273d73ac8fff
s1f102100793 <s1f102100793@iniad.org>
2024-03-08 16:52:20 +0900

    chore: replace calculateMonthlyStatistics and updateChartData to stastics.ts

--- src/components/OptionStatistics/OptionStatistics.tsx
+++ src/components/OptionStatistics/OptionStatistics.tsx
@@ -11,19 +11,15 @@ import {
   type ChartData,
   type ChartOptions
 } from "chart.js"
-import {
-  addMonths,
-  eachDayOfInterval,
-  endOfMonth,
-  format,
-  isAfter,
-  isBefore,
-  startOfMonth
-} from "date-fns"
+import { addMonths, format, isAfter, isBefore, startOfMonth } from "date-fns"
 import React, { useEffect, useState } from "react"
 import { Line } from "react-chartjs-2"
 import { getCountData } from "src/utils/dailyCount"
 import { findOldestDataMonth } from "src/utils/date-fns"
+import {
+  calculateMonthlyStatistics,
+  updateChartData
+} from "src/utils/statistics"
 
 import styles from "./OptionStatistics.module.css"
 
@@ -37,36 +33,6 @@ ChartJS.register(
   Legend,
   Filler
 )
-const calculateMonthlyStatistics = (
-  allCounts: Record<string, Record<string, number>>,
-  targetMonth: Date
-) => {
-  const startOfTargetMonth = startOfMonth(targetMonth)
-  const endOfTargetMonth = endOfMonth(targetMonth)
-
-  let totalQuestions = 0
-  let daysWithQuestions = 0
-
-  Object.entries(allCounts).forEach(([dateStr, counts]) => {
-    const date = new Date(dateStr)
-    if (date >= startOfTargetMonth && date <= endOfTargetMonth) {
-      const dailyTotal = Object.values(counts).reduce(
-        (acc, count) => acc + count,
-        0
-      )
-      totalQuestions += dailyTotal
-      if (dailyTotal > 0) {
-        daysWithQuestions++
-      }
-    }
-  })
-
-  const averageQuestionsPerActiveDay =
-    daysWithQuestions > 0 ? totalQuestions / daysWithQuestions : 0
-
-  const ratioToPreviousMonth = null
-  return { totalQuestions, averageQuestionsPerActiveDay, ratioToPreviousMonth }
-}
 
 const OptionStatistics = () => {
   const [currentDate, setCurrentDate] = useState(new Date())
@@ -115,46 +81,13 @@ const OptionStatistics = () => {
     fetchData()
   }, [currentDate])
 
-  const updateChartData = async (date: Date) => {
-    const allCounts = await getCountData()
-    const today = new Date()
-    const startDate = startOfMonth(date)
-    const endDate = endOfMonth(date)
-    const eachDay = eachDayOfInterval({ start: startDate, end: endDate })
-    const labels = eachDay.map((day) => format(day, "yyyy-MM-dd"))
-    const model3Data = labels.map((date) =>
-      isAfter(new Date(date), today) ? null : allCounts[date]?.["3.5"] || 0
-    )
-    const model4Data = labels.map((date) =>
-      isAfter(new Date(date), today) ? null : allCounts[date]?.["4"] || 0
-    )
-
-    setData({
-      labels,
-      datasets: [
-        {
-          label: "Model 3.5",
-          data: model3Data,
-          borderColor: "rgb(24, 195, 125)",
-          backgroundColor: "rgba(24, 195, 125, 0.5)",
-          tension: 0.3,
-          stack: "1",
-          fill: "origin"
-        },
-        {
-          label: "Model 4",
-          data: model4Data,
-          borderColor: "rgb(153, 102, 255)",
-          backgroundColor: "rgba(153, 102, 255, 0.5)",
-          stack: "1",
-          tension: 0.3,
-          fill: "-1"
-        }
-      ]
+  const getChartData = async (date: Date) => {
+    await updateChartData(date).then((chartData) => {
+      setData(chartData)
     })
   }
   useEffect(() => {
-    updateChartData(currentDate)
+    getChartData(currentDate)
     setOptions((currentOptions) => ({
       ...currentOptions,
       plugins: {
---
+++ src/utils/statistics.ts
@@ -0,0 +1,81 @@
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  isAfter,
+  startOfMonth
+} from "date-fns"
+
+import { getCountData } from "./dailyCount"
+
+export const calculateMonthlyStatistics = (
+  allCounts: Record<string, Record<string, number>>,
+  targetMonth: Date
+) => {
+  const startOfTargetMonth = startOfMonth(targetMonth)
+  const endOfTargetMonth = endOfMonth(targetMonth)
+
+  let totalQuestions = 0
+  let daysWithQuestions = 0
+
+  Object.entries(allCounts).forEach(([dateStr, counts]) => {
+    const date = new Date(dateStr)
+    if (date >= startOfTargetMonth && date <= endOfTargetMonth) {
+      const dailyTotal = Object.values(counts).reduce(
+        (acc, count) => acc + count,
+        0
+      )
+      totalQuestions += dailyTotal
+      if (dailyTotal > 0) {
+        daysWithQuestions++
+      }
+    }
+  })
+
+  const averageQuestionsPerActiveDay =
+    daysWithQuestions > 0 ? totalQuestions / daysWithQuestions : 0
+
+  const ratioToPreviousMonth = null
+  return { totalQuestions, averageQuestionsPerActiveDay, ratioToPreviousMonth }
+}
+
+export const updateChartData = async (date: Date) => {
+  const allCounts = await getCountData()
+  const today = new Date()
+  const startDate = startOfMonth(date)
+  const endDate = endOfMonth(date)
+  const eachDay = eachDayOfInterval({ start: startDate, end: endDate })
+  const labels = eachDay.map((day) => format(day, "yyyy-MM-dd"))
+  const model3Data = labels.map((date) =>
+    isAfter(new Date(date), today) ? null : allCounts[date]?.["3.5"] || 0
+  )
+  const model4Data = labels.map((date) =>
+    isAfter(new Date(date), today) ? null : allCounts[date]?.["4"] || 0
+  )
+
+  console.log({ labels, model3Data, model4Data })
+
+  return {
+    labels,
+    datasets: [
+      {
+        label: "Model 3.5",
+        data: model3Data,
+        borderColor: "rgb(24, 195, 125)",
+        backgroundColor: "rgba(24, 195, 125, 0.5)",
+        tension: 0.3,
+        stack: "1",
+        fill: "origin"
+      },
+      {
+        label: "Model 4",
+        data: model4Data,
+        borderColor: "rgb(153, 102, 255)",
+        backgroundColor: "rgba(153, 102, 255, 0.5)",
+        stack: "1",
+        tension: 0.3,
+        fill: "-1"
+      }
+    ]
+  }
+}
