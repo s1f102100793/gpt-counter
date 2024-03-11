259088fd7a57659f0bd33859bcddf228a0dda4bd
s1f102100793 <s1f102100793@iniad.org>
2024-03-11 18:51:44 +0900

    fix: Uncaught (in promise) Error: Could not establish connection. Receiving end does not exist

--- src/background/messages/url.ts
+++ src/background/messages/url.ts
@@ -1,8 +1,12 @@
+const targetUrl = "https://chat.openai.com/c/"
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.url !== undefined) {
-    chrome.tabs.sendMessage(tabId, {
-      name: "url",
-      body: { url: changeInfo.url }
-    })
+    if (changeInfo.url.startsWith(targetUrl)) {
+      chrome.tabs.sendMessage(tabId, {
+        name: "url",
+        body: { url: changeInfo.url }
+      })
+    }
   }
 })
